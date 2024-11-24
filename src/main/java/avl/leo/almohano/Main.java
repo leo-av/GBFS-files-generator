b4c39b8aaff2da41ce65beccaa278ff6675729d5
Leo Almohano <leo.almohano@inetum.com>
2024-11-24 18:10:36 +0100

    Update Main.java

--- src/main/java/avl/leo/almohano/Main.java
+++ src/main/java/avl/leo/almohano/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int iteration = 1;
+        int iteration = 0;
 
         do {
             // Ask the user for the operator name
